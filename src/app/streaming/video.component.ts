6c457306e925dca8aa8d1a7ac3ff1977a6c73e62
Deniz.Karadag <deniz.karadag@finaps.nl>
2023-06-26 16:33:19 +0200

    adaptive streaming works

--- src/app/streaming/video.component.ts
+++ src/app/streaming/video.component.ts
@@ -1,43 +1,56 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import videojs from 'video.js';
 
 @Component({
   selector: 'app-vjs-player',
   templateUrl: './video.component.html',
 })
-export class VjsPlayerComponent implements AfterViewInit {
+export class VjsPlayerComponent implements AfterViewInit, OnInit {
   @ViewChild('videoPlayer', { static: true }) videoPlayerRef!: ElementRef;
+  movieId: string | null = null;
+  // need to create constructor and ngOnit so we can read the path parameter from the URL
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.movieId = this.route.snapshot.paramMap.get('movie');
+    console.log(this.movieId);
+  }
 
   ngAfterViewInit(): void {
     const videoPlayer = videojs(this.videoPlayerRef.nativeElement, {
       autoplay: true,
       controls: true,
-      fluid: true,
+      controlBar: {
+        volumePanel: {
+          inline: false,
+        },
+        skipButtons: {
+          forward: 10,
+          backward: 10,
+        },
+      },
+      muted: true,
+      // fluid: true,
       aspectRatio: '16:9',
       playbackRates: [0.5, 1, 1.5, 2],
+      VideoPlaybackQuality: {
+        bandwidth: 800000,
+      },
+      sources: [
+        {
+          src: 'http://localhost:8080/videos/' + this.movieId + '/master.m3u8',
+          type: 'application/x-mpegURL',
+          withCredentials: true,
+        },
+      ],
     });
-
-    videoPlayer.src([
-      {
-        src: 'http://localhost:8080/videos/reloaded/output.mp4',
-        type: 'video/mp4',
-      },
-      {
-        src: 'http://localhost:8080/videos/reloaded/output_720p.m3u8',
-        type: 'application/x-mpegURL',
-        label: '720p',
-      },
-      {
-        src: 'http://localhost:8080/videos/reloaded/output_480p.m3u8',
-        type: 'application/x-mpegURL',
-        label: '480p',
-      },
-      {
-        src: 'http://localhost:8080/videos/reloaded/output_360p.m3u8',
-        type: 'application/x-mpegURL',
-        label: '360p',
-      },
-    ]);
 
     videoPlayer.play();
   }
