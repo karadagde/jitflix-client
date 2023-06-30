a970c69d6d3d30a2e3e3b8052ee4f55b7edb571e
Deniz.Karadag <deniz.karadag@finaps.nl>
2023-06-30 15:32:42 +0200

    implemented working file upload

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,21 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FileUploadService } from './file-upload.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { tap, forkJoin, Observable, Observer, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FileUploadComponent {
   @ViewChild('fileUpload') fileUpload!: ElementRef<HTMLInputElement>;
-  constructor(private readonly service: FileUploadService) {}
+
+  private readonly uploadProgressSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  public readonly uploadProgress$: Observable<number> =
+    this.uploadProgressSubject.asObservable();
+
+  constructor(private fileUploadService: FileUploadService) {}
 
   uploadFile() {
     const files = this.fileUpload.nativeElement.files;
@@ -31,15 +32,61 @@ export class FileUploadComponent {
         ) {
           return;
         }
-        const content = event.target.result;
-        const size = 1000;
-        const totalChunks = content.byteLength / size;
-        const fileName = Math.random().toString(36).slice(-6) + files[0].name;
+        const content = event.target.result as ArrayBuffer;
+        const size = 1000000;
+        const totalChunks = Math.ceil(content.byteLength / size);
+        const fileName = files[0].name;
+        // const fileName = Math.random().toString(36).slice(-6) + files[0].name;
 
-        for (let i = 0; i < totalChunks + 1; i++) {
+        const uploadRequests = [];
+        for (let i = 0; i < totalChunks; i++) {
           const chunk = content.slice(i * size, (i + 1) * size);
-          this.service.uploadFile(chunk, fileName, i, totalChunks);
+          uploadRequests.push(
+            this.fileUploadService.uploadFile(
+              chunk,
+              fileName,
+              i,
+              totalChunks,
+              size
+            )
+          );
         }
+
+        const progressObservables = uploadRequests.map(
+          (request) =>
+            new Observable((observer: Observer<HttpEvent<any>>) => {
+              request.subscribe({
+                next: (event: HttpEvent<any>) => {
+                  if (
+                    event.type === HttpEventType.UploadProgress &&
+                    event.total
+                  ) {
+                    this.uploadProgressSubject.next(
+                      Math.round((100 * event.loaded) / event.total)
+                    );
+                  }
+                  observer.next(event);
+                },
+                error: (error: any) => observer.error(error),
+                complete: () => observer.complete(),
+              });
+            })
+        );
+
+        forkJoin(progressObservables).subscribe({
+          next: (results: HttpEvent<any>[]) => {
+            // All chunks have been uploaded
+            console.log('Upload completed');
+          },
+          error: (error: any) => {
+            // Handle error
+            console.log('Upload error:', error);
+          },
+          complete: () => {
+            // Handle completion
+            console.log('Upload process completed');
+          },
+        });
       };
     }
   }
--- src/app/file-upload/file-upload.service.ts
+++ src/app/file-upload/file-upload.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, tap } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class FileUploadService {
@@ -10,25 +10,26 @@ export class FileUploadService {
     file: ArrayBuffer,
     id: string,
     chunk: number,
-    totalChunks: number
-  ) {
-    this.http
-      .post('http://localhost:8080/videos/upload/large-file' + id, file, {
-        headers: {
-          'Content-Type': 'application/octet-stream',
-          'Content-Range': `bytes ${chunk}-${
-            chunk + file.byteLength
-          }/${totalChunks}`,
-          'Content-Length': `${file.byteLength}`,
-        },
-      })
-      .pipe(
-        tap((res) => console.log(res)),
-        catchError((err) => {
-          console.log(err);
-          throw err;
-        })
-      )
-      .subscribe();
+    totalChunks: number,
+    size: number
+  ): Observable<HttpEvent<any>> {
+    const start = chunk * size;
+    const end = start + (file.byteLength - 1);
+    const contentRange = `bytes ${start}-${end}/${totalChunks * size}`;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/octet-stream',
+      'Content-Range': contentRange,
+    });
+
+    return this.http.post(
+      `http://localhost:8080/videos/upload/large-file/${id}`,
+      file,
+      {
+        headers,
+        reportProgress: true,
+        observe: 'events',
+      }
+    );
   }
 }
